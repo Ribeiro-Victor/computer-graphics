3a527e214787fbf0ef277cfb3dddd6e2d13c6063
Victor Ribeiro <ribeir145@gmail.com>
2023-06-11 22:45:34 -0300

    atualiza tapaway

--- Trabalho 02/pick2.py
+++ Trabalho 02/pick2.py
@@ -2,6 +2,10 @@ import sys
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 from OpenGL.GL import *
+from arcball import ArcBall
+from math import degrees
+from texturedcube import drawCube, loadTexture
+import random
 
 # Selected object
 selected = None
@@ -15,33 +19,201 @@ n = 3
 # rotation angle
 angle = 0
 
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 1000
+
+possible_directions = { 0: [0, 0, 0, 0],    # Cima
+                        1: [180, 1, 0, 0],  # Baixo
+                        2: [90, 0, 0, 1],   # Esquerda
+                        3: [90, 0, 0, -1],  # Direita
+                        4: [90, 1, 0, 0],   # Frente
+                        5: [90, -1, 0, 0],  # Trás
+                    }
+
+cubes_directions={}
+# graph_cubes_dependency={'a':['b', 'c'],
+#                         'b':['e', 'f'],
+#                         'c':['g', 'd'],
+#                         'd':['b', 'f'],
+#                         'g':[],
+#                         'f':[],
+#                         'e':['f']}
+graph_cubes_dependency = {}
+# A -> B, significa que A precisa de B para ser liberado.
+
+def find_cube_coords(name):
+    # Acha as coordenas i, j, k do cubinho
+    common_multiplier = n**(n-1) 
+    i = name//(common_multiplier)
+    j = (name-(i*common_multiplier)) // n
+    k = (name-(i*common_multiplier)-(j*n)) % n #Só funciona para n = 3 por enquanto
+    # print("i j k:", i, j, k)
+
+    return [i, j, k]
+
+def possible_blocking_cubes(name, direction):
+
+    coords = find_cube_coords(name)
+    i = coords[0]
+    j = coords[1]
+    k = coords[2]
+    # direction = cubes_directions.get(name)
+
+    if(direction[0] != 90): # Direção é cima ou baixo
+        # Variação do J
+        possible_blockings = [(i*n + j_var) * n + k for j_var in range(n)]
+        possible_blockings.remove(name)
+        if(direction[1] > 0): # Baixo
+            blocking_cubes = [x for x in possible_blockings if x < name]
+        else: # Cima
+            blocking_cubes = [x for x in possible_blockings if x > name]
+        
+    elif(direction[1] == 0): # Direção é direita ou esquerda
+        # Variação do I
+        possible_blockings = [(i_var*n + j) * n + k for i_var in range(n)]
+        possible_blockings.remove(name)
+        if(direction[3] > 0): # Esquerda
+            blocking_cubes = [x for x in possible_blockings if x < name]
+        else: # Direita
+            blocking_cubes = [x for x in possible_blockings if x > name]
+
+    else: # Direção é frente ou trás
+        # Variação do K
+        possible_blockings = [(i*n + j) * n + k_var for k_var in range(n)]
+        possible_blockings.remove(name)
+        if(direction[1] > 0): # Frente
+            blocking_cubes = [x for x in possible_blockings if x > name]
+        else: # Trás
+            blocking_cubes = [x for x in possible_blockings if x < name]
+    
+    return blocking_cubes
+
+
+def init_cube_directions():
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                name = (i*n + j) * n + k
+                graph_cubes_dependency[name] = []
+    
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                name = (i*n + j) * n + k
+                random_n = random.randint(0, 5)
+                temp_direction = possible_directions.get(random_n)
+                temp_blockings = possible_blocking_cubes(name, temp_direction)
+                while(not DFS(temp_blockings, name)):
+                    random_n += 1
+                    temp_direction = possible_directions.get(random_n%6)
+                    temp_blockings = possible_blocking_cubes(name, temp_direction)
+                    
+                graph_cubes_dependency[name] = temp_blockings
+                cubes_directions[name] = temp_direction
+
+def DFS_Util(v, visited):
+    visited.add(v)
+    for u in graph_cubes_dependency.get(v):
+        if u not in visited:
+            DFS_Util(u, visited)
+
+def DFS(starts: list, target):
+    # Função DFS que checa se é possível criar dependencia entre vértices sem formar ciclo
+    for start in starts:
+        visited = set()
+        DFS_Util(start, visited)
+        if(target in visited):
+            return False
+    return True # Significa que não formará ciclo
+
+# DFS(['e', 'c'],'a')
+                
+
+init_cube_directions()
+print(graph_cubes_dependency)
+# print(cubes_directions)
+
+
+
+def possible_checkout(name):
+    
+    # blocking_cubes = possible_blocking_cubes(name, cubes_directions.get(name))
+    blocking_cubes = graph_cubes_dependency.get(name)
+    blocking_cubes = [x for x in blocking_cubes if x not in removed]
+
+    # print('possible blocking_cubes', possible_blockings)
+    # print('blocking_cubes', blocking_cubes)
+    if(len(blocking_cubes)==0):
+        return True
+    else:
+        return False
+    
+
+def draw_you_win():
+    glDisable(GL_TEXTURE_2D)
+    loadTexture("youwin.png")
+    glEnable(GL_TEXTURE_2D)
+    glBegin(GL_QUADS)
+    glTexCoord2f(0.0, 0.0)
+    glVertex2f(-1, -1)
+    glTexCoord2f(1.0, 0.0)
+    glVertex2f(1, -1)
+    glTexCoord2f(1.0, 1.0)
+    glVertex2f(1, 1)
+    glTexCoord2f(0.0, 1.0)
+    glVertex2f(-1, 1)
+    glEnd()
+    pass
+
 def draw_scene(flatColors = False):
     "Draws the scene emitting a 'name' for each cube"
     glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glMatrixMode (GL_MODELVIEW)
     glLoadIdentity()
     glTranslatef(0,0,-3)
-    glRotatef (-80, 1,0,0)
-    glRotatef (angle,0,1,1)
+    glMultMatrixd (matrix)
+    # glRotatef (-80, 1,0,0)
+    # glRotatef (angle,0,1,1)
+    counter = 0
     size = 1/n
-    for i in range(n):
-        x = i - (n-1)/2 
-        for j in range(n):
-            y = j - (n-1)/2 
-            for k in range(n):
-                z = k - (n-1)/2 
-                name = (i*n + j) * n + k
-                if flatColors:
-                    glColor3f((i+1)/n, (j+1)/n, (k+1)/n)
-                # Ignore removed objects
-                if name in removed: continue 
-                glLoadName(name)
-                glPushMatrix()
-                glTranslatef(x*size,y*size,z*size)
-                glutSolidCube(size*0.8)
-                glPopMatrix()
+    if(len(removed)<n**(3)):
+    # if(False):
+        for i in range(n):
+            x = i - (n-1)/2 
+            for j in range(n):
+                y = j - (n-1)/2 
+                for k in range(n):
+                    z = k - (n-1)/2 
+                    name = (i*n + j) * n + k
+                    # print(name)
+                    if flatColors:
+                        glColor3f((i+1)/n, (j+1)/n, (k+1)/n)
+                    # glColor3f(1, 1, 0)
+                    # Ignore removed objects
+                    if name in removed: continue 
+                    glLoadName(name)
+                    glPushMatrix()
+                    if(name == 2):
+                        # print('display')
+                        aa = 0.2
+                    else:
+                        aa = 0
+                    glTranslatef((x*size),y*size,z*size)
+                    # glutSolidCube(size*0.8)
+                    direction = cubes_directions.get(name)
+                    
+                    glRotatef(direction[0], direction[1], direction[2], direction[3])
+                    drawCube(0.4*size)
+                    glPopMatrix()
+    else:
+        # glPushMatrix()
+        # glutSolidCube(1)
+        draw_you_win()
+        # glPopMatrix()
             
 def display():
+   
+    # glMultMatrixd (matrix)
     draw_scene();
     glutSwapBuffers ();
 
@@ -57,6 +229,12 @@ def init ():
     # Helps with antialiasing
     glEnable(GL_MULTISAMPLE)
 
+    textureId = loadTexture("arrow2.jpg")
+    glEnable(GL_TEXTURE_2D)
+
+    global matrix 
+    matrix = glGetDoublev(GL_MODELVIEW_MATRIX)
+
 
 def reshape(width,height):
     glMatrixMode (GL_PROJECTION)
@@ -69,9 +247,11 @@ def reshape(width,height):
  
 def pick(x,y):
     glDisable(GL_LIGHTING)
+    glDisable(GL_TEXTURE_2D)
     draw_scene(True)
     glFlush()
     glEnable (GL_LIGHTING)
+    glEnable(GL_TEXTURE_2D)
     buf = glReadPixels (x,windowSize[1]-y,1,1,GL_RGB,GL_FLOAT)
     pixel = buf[0][0]
     r,g,b = pixel
@@ -84,9 +264,51 @@ def mousePressed(button,state,x,y):
     if state == GLUT_DOWN:
         global prevx, prevy, prevz 
         prevx,prevy = x,y
+        global startx, starty
+        startx, starty = x,y
         selected = pick(x,y)
+        # print("pressed:", x, y)
         if selected >= 0:
-            removed.add(selected)
+            print('selected:', selected)
+    
+            # glLoadIdentity()
+            # glLoadName(selected)
+            # glPushMatrix()
+            # glTranslatef(100, 0, 0)
+            # drawCube()
+            # glPopMatrix()
+            if(possible_checkout(selected)):
+                removed.add(selected)
+                print('removido')
+            else:
+                print('existem cubos bloqueantes')
+
+        # else:
+        #     global arcball
+        #     arcball = ArcBall ((width/2,height/2,0), width/2)
+        #     global startx, starty
+        #     startx, starty = x,y
+        #     glutMotionFunc (rotatecallback)
+    glutPostRedisplay()
+
+# def mousepressArc (button, state, x, y):
+def mousepressArc (x, y):
+
+    global arcball
+    arcball = ArcBall ((WINDOW_WIDTH/2,WINDOW_HEIGHT/2,0), WINDOW_WIDTH/2)
+    global startx, starty
+    startx, starty = x,y
+    glutMotionFunc (rotatecallback)
+
+            
+def rotatecallback (x, y):
+    global startx,starty,matrix
+    angle, axis = arcball.rot (startx, WINDOW_WIDTH - starty, x, WINDOW_HEIGHT - y)
+    glLoadIdentity ()
+    glRotatef (degrees(angle),*axis)
+    glMultMatrixd (matrix)
+    matrix = glGetDoublev(GL_MODELVIEW_MATRIX)
+    startx,starty = x,y
     glutPostRedisplay()
 
 def idle():
@@ -98,14 +320,17 @@ def idle():
 def main():
     glutInit(sys.argv);
     glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_MULTISAMPLE);
-    glutInitWindowSize (400, 400); 
-    glutInitWindowPosition (100, 100);
+    glutInitWindowSize (WINDOW_WIDTH, WINDOW_HEIGHT); 
+    # glutInitWindowPosition((glutGet(GLUT_SCREEN_WIDTH)-WINDOW_WIDTH)//2, (glutGet(GLUT_SCREEN_HEIGHT)-WINDOW_HEIGHT)//2)
+    glutInitWindowPosition(((glutGet(GLUT_SCREEN_WIDTH)-WINDOW_WIDTH)//2)+400, (glutGet(GLUT_SCREEN_HEIGHT)-WINDOW_HEIGHT)//2)
     glutCreateWindow ("picking");
     init ();
     glutReshapeFunc(reshape)
     glutDisplayFunc(display)
     glutMouseFunc(mousePressed)
-    glutIdleFunc(idle)
+    glutMotionFunc(mousepressArc)
+    # glutIdleFunc(idle)
+    # glutMouseFunc(mousepressArc)
     glutMainLoop();
 
 main()
--- Trabalho 02/texturedcube.py
+++ Trabalho 02/texturedcube.py
@@ -3,6 +3,8 @@ from OpenGL.GLUT import *
 from OpenGL.GLU import *
 import sys
 from PIL  import Image
+from arcball import ArcBall
+from math import degrees
 
 def loadTexture (filename):
     "Loads an image from a file as a texture"
@@ -30,131 +32,257 @@ def loadTexture (filename):
 
     return image
 
-def drawCube():
+def drawCube(size = 0.5):
     glBegin(GL_QUADS)  # Start Drawing The Cube
+
+    # x y z
+    direction = [0, 1, 0]
 
     # Front Face (note that the texture's corners have to match the quad's corners)
     glNormal3f(0,0,1)
     glTexCoord2f(0.0, 0.0)
-    glVertex3f(-1.0, -1.0, 1.0)  # Bottom Left Of The Texture and Quad
+    glVertex3f(-1.0*size, -1.0*size, 1.0*size)  # Bottom Left Of The Texture and Quad
     glTexCoord2f(1.0, 0.0)
-    glVertex3f(1.0, -1.0, 1.0)  # Bottom Right Of The Texture and Quad
+    glVertex3f(1.0*size, -1.0*size, 1.0*size)  # Bottom Right Of The Texture and Quad
     glTexCoord2f(1.0, 1.0)
-    glVertex3f(1.0, 1.0, 1.0)  # Top Right Of The Texture and Quad
+    glVertex3f(1.0*size, 1.0*size, 1.0*size)  # Top Right Of The Texture and Quad
     glTexCoord2f(0.0, 1.0)
-    glVertex3f(-1.0, 1.0, 1.0)  # Top Left Of The Texture and Quad
+    glVertex3f(-1.0*size, 1.0*size, 1.0*size)  # Top Left Of The Texture and Quad
 
     # Back Face
     glNormal3f(0,0,-1)
     glTexCoord2f(1.0, 0.0)
-    glVertex3f(-1.0, -1.0, -1.0)  # Bottom Right Of The Texture and Quad
+    glVertex3f(-1.0*size, -1.0*size, -1.0*size)  # Bottom Right Of The Texture and Quad
     glTexCoord2f(1.0, 1.0)
-    glVertex3f(-1.0, 1.0, -1.0)  # Top Right Of The Texture and Quad
+    glVertex3f(-1.0*size, 1.0*size, -1.0*size)  # Top Right Of The Texture and Quad
     glTexCoord2f(0.0, 1.0)
-    glVertex3f(1.0, 1.0, -1.0)  # Top Left Of The Texture and Quad
+    glVertex3f(1.0*size, 1.0*size, -1.0*size)  # Top Left Of The Texture and Quad
     glTexCoord2f(0.0, 0.0)
-    glVertex3f(1.0, -1.0, -1.0)  # Bottom Left Of The Texture and Quad
+    glVertex3f(1.0*size, -1.0*size, -1.0*size)  # Bottom Left Of The Texture and Quad
 
     # Top Face
     glNormal3f(0,1,0)
-    glTexCoord2f(0.0, 1.0)
-    glVertex3f(-1.0, 1.0, -1.0)  # Top Left Of The Texture and Quad
-    glTexCoord2f(0.0, 0.0)
-    glVertex3f(-1.0, 1.0, 1.0)  # Bottom Left Of The Texture and Quad
-    glTexCoord2f(1.0, 0.0)
-    glVertex3f(1.0, 1.0, 1.0)  # Bottom Right Of The Texture and Quad
     glTexCoord2f(1.0, 1.0)
-    glVertex3f(1.0, 1.0, -1.0)  # Top Right Of The Texture and Quad
+    glVertex3f(-1.0*size, 1.0*size, -1.0*size)  # Top Left Of The Texture and Quad
+    # glTexCoord2f(0.0, 0.0)
+    glTexCoord2f(0.8, 1.0)
+    glVertex3f(-1.0*size, 1.0*size, 1.0*size)  # Bottom Left Of The Texture and Quad
+    # glTexCoord2f(1.0*size, 0.0)
+    glTexCoord2f(0.8, 0.8)
+    glVertex3f(1.0*size, 1.0*size, 1.0*size)  # Bottom Right Of The Texture and Quad
+    # glTexCoord2f(1.0*size, 1.0*size)
+    glTexCoord2f(1.0, 0.8)
+    glVertex3f(1.0*size, 1.0*size, -1.0*size)  # Top Right Of The Texture and Quad
 
     # Bottom Face
     glNormal3f(0,-1,0)
-    glTexCoord2f(1.0, 1.0)
-    glVertex3f(-1.0, -1.0, -1.0)  # Top Right Of The Texture and Quad
-    glTexCoord2f(0.0, 1.0)
-    glVertex3f(1.0, -1.0, -1.0)  # Top Left Of The Texture and Quad
-    glTexCoord2f(0.0, 0.0)
-    glVertex3f(1.0, -1.0, 1.0)  # Bottom Left Of The Texture and Quad
-    glTexCoord2f(1.0, 0.0)
-    glVertex3f(-1.0, -1.0, 1.0)  # Bottom Right Of The Texture and Quad
+    # glTexCoord2f(1.0*size, 1.0*size)
+    glTexCoord2f(0.4, 0.0)
+    glVertex3f(-1.0*size, -1.0*size, -1.0*size)  # Top Right Of The Texture and Quad
+    # glTexCoord2f(0.0, 1.0*size)
+    glTexCoord2f(0.6, 0.0)
+    glVertex3f(1.0*size, -1.0*size, -1.0*size)  # Top Left Of The Texture and Quad
+    # glTexCoord2f(0.0, 0.0)
+    glTexCoord2f(0.6, 0.2)
+    glVertex3f(1.0*size, -1.0*size, 1.0*size)  # Bottom Left Of The Texture and Quad
+    # glTexCoord2f(1.0*size, 0.0)
+    glTexCoord2f(0.4, 0.2)
+    glVertex3f(-1.0*size, -1.0*size, 1.0*size)  # Bottom Right Of The Texture and Quad
 
     # Right face
     glNormal3f(1,0,0)
     glTexCoord2f(1.0, 0.0)
-    glVertex3f(1.0, -1.0, -1.0)  # Bottom Right Of The Texture and Quad
+    glVertex3f(1.0*size, -1.0*size, -1.0*size)  # Bottom Right Of The Texture and Quad
     glTexCoord2f(1.0, 1.0)
-    glVertex3f(1.0, 1.0, -1.0)  # Top Right Of The Texture and Quad
+    glVertex3f(1.0*size, 1.0*size, -1.0*size)  # Top Right Of The Texture and Quad
     glTexCoord2f(0.0, 1.0)
-    glVertex3f(1.0, 1.0, 1.0)  # Top Left Of The Texture and Quad
+    glVertex3f(1.0*size, 1.0*size, 1.0*size)  # Top Left Of The Texture and Quad
     glTexCoord2f(0.0, 0.0)
-    glVertex3f(1.0, -1.0, 1.0)  # Bottom Left Of The Texture and Quad
+    glVertex3f(1.0*size, -1.0*size, 1.0*size)  # Bottom Left Of The Texture and Quad
 
     # Left Face
     glNormal3f(-1,0,0)
+    glColor3f(1.0, 1.0, 0.0)
     glTexCoord2f(0.0, 0.0)
-    glVertex3f(-1.0, -1.0, -1.0)  # Bottom Left Of The Texture and Quad
+    glVertex3f(-1.0*size, -1.0*size, -1.0*size)  # Bottom Left Of The Texture and Quad
     glTexCoord2f(1.0, 0.0)
-    glVertex3f(-1.0, -1.0, 1.0)  # Bottom Right Of The Texture and Quad
+    glVertex3f(-1.0*size, -1.0*size, 1.0*size)  # Bottom Right Of The Texture and Quad
     glTexCoord2f(1.0, 1.0)
-    glVertex3f(-1.0, 1.0, 1.0)  # Top Right Of The Texture and Quad
+    glVertex3f(-1.0*size, 1.0*size, 1.0*size)  # Top Right Of The Texture and Quad
     glTexCoord2f(0.0, 1.0)
-    glVertex3f(-1.0, 1.0, -1.0)  # Top Left Of The Texture and Quad
+    glVertex3f(-1.0*size, 1.0*size, -1.0*size)  # Top Left Of The Texture and Quad
 
     glEnd()
 
-# Screen width and height
-width,height = 500,500
 
-# Current rotation angle in degrees
-angle = 0
+def drawCube2(size = 0.5):
+    glBegin(GL_QUADS)  # Start Drawing The Cube
 
-def init():
-    """Initialize state"""
-    glEnable(GL_DEPTH_TEST)
-    glEnable(GL_LIGHTING)
-    glEnable(GL_LIGHT0)
+    # x y z
+    direction = [0, 1, 0]
 
-    # Helps with antialiasing
-    glEnable(GL_MULTISAMPLE)
+    # Front Face (note that the texture's corners have to match the quad's corners)
+    glNormal3f(0,0,1)
+    glTexCoord2f(0.0, 0.0)
+    glVertex3f(-1.0*size, -1.0*size, 1.0*size)  # Bottom Left Of The Texture and Quad
+    glTexCoord2f(1.0, 0.0)
+    glVertex3f(1.0*size, -1.0*size, 1.0*size)  # Bottom Right Of The Texture and Quad
+    glTexCoord2f(1.0, 1.0)
+    glVertex3f(1.0*size, 1.0*size, 1.0*size)  # Top Right Of The Texture and Quad
+    glTexCoord2f(0.0, 1.0)
+    glVertex3f(-1.0*size, 1.0*size, 1.0*size)  # Top Left Of The Texture and Quad
 
-    # Texture initialization
-    textureId = loadTexture("arrow.jpg")
-    glEnable(GL_TEXTURE_2D)
+    # Back Face
+    glNormal3f(0,0,-1)
+    glTexCoord2f(1.0, 0.0)
+    glVertex3f(-1.0*size, -1.0*size, -1.0*size)  # Bottom Right Of The Texture and Quad
+    glTexCoord2f(1.0, 1.0)
+    glVertex3f(-1.0*size, 1.0*size, -1.0*size)  # Top Right Of The Texture and Quad
+    glTexCoord2f(0.0, 1.0)
+    glVertex3f(1.0*size, 1.0*size, -1.0*size)  # Top Left Of The Texture and Quad
+    glTexCoord2f(0.0, 0.0)
+    glVertex3f(1.0*size, -1.0*size, -1.0*size)  # Bottom Left Of The Texture and Quad
 
+    # Top Face
+    glNormal3f(0,1,0)
+    glTexCoord2f(1.0, 1.0)
+    glVertex3f(-1.0*size, 1.0*size, -1.0*size)  # Top Left Of The Texture and Quad
+    # glTexCoord2f(0.0, 0.0)
+    glTexCoord2f(0.8, 1.0)
+    glVertex3f(-1.0*size, 1.0*size, 1.0*size)  # Bottom Left Of The Texture and Quad
+    # glTexCoord2f(1.0*size, 0.0)
+    glTexCoord2f(0.8, 0.8)
+    glVertex3f(1.0*size, 1.0*size, 1.0*size)  # Bottom Right Of The Texture and Quad
+    # glTexCoord2f(1.0*size, 1.0*size)
+    glTexCoord2f(1.0, 0.8)
+    glVertex3f(1.0*size, 1.0*size, -1.0*size)  # Top Right Of The Texture and Quad
+
+    # Bottom Face
+    glNormal3f(0,-1,0)
+    # glTexCoord2f(1.0*size, 1.0*size)
+    glTexCoord2f(0.4, 0.0)
+    glVertex3f(-1.0*size, -1.0*size, -1.0*size)  # Top Right Of The Texture and Quad
+    # glTexCoord2f(0.0, 1.0*size)
+    glTexCoord2f(0.6, 0.0)
+    glVertex3f(1.0*size, -1.0*size, -1.0*size)  # Top Left Of The Texture and Quad
+    # glTexCoord2f(0.0, 0.0)
+    glTexCoord2f(0.6, 0.2)
+    glVertex3f(1.0*size, -1.0*size, 1.0*size)  # Bottom Left Of The Texture and Quad
+    # glTexCoord2f(1.0*size, 0.0)
+    glTexCoord2f(0.4, 0.2)
+    glVertex3f(-1.0*size, -1.0*size, 1.0*size)  # Bottom Right Of The Texture and Quad
+
+    # Right face
+    glNormal3f(1,0,0)
+    glTexCoord2f(1.0, 0.0)
+    glVertex3f(1.0*size, -1.0*size, -1.0*size)  # Bottom Right Of The Texture and Quad
+    glTexCoord2f(1.0, 1.0)
+    glVertex3f(1.0*size, 1.0*size, -1.0*size)  # Top Right Of The Texture and Quad
+    glTexCoord2f(0.0, 1.0)
+    glVertex3f(1.0*size, 1.0*size, 1.0*size)  # Top Left Of The Texture and Quad
+    glTexCoord2f(0.0, 0.0)
+    glVertex3f(1.0*size, -1.0*size, 1.0*size)  # Bottom Left Of The Texture and Quad
+
+    # Left Face
+    glNormal3f(-1,0,0)
+    glColor3f(1.0, 1.0, 0.0)
+    glTexCoord2f(0.0, 0.0)
+    glVertex3f(-1.0*size, -1.0*size, -1.0*size)  # Bottom Left Of The Texture and Quad
+    glTexCoord2f(1.0, 0.0)
+    glVertex3f(-1.0*size, -1.0*size, 1.0*size)  # Bottom Right Of The Texture and Quad
+    glTexCoord2f(1.0, 1.0)
+    glVertex3f(-1.0*size, 1.0*size, 1.0*size)  # Top Right Of The Texture and Quad
+    glTexCoord2f(0.0, 1.0)
+    glVertex3f(-1.0*size, 1.0*size, -1.0*size)  # Top Left Of The Texture and Quad
+
+    glEnd()
+
+
+if __name__=='__main__':
+    # Screen width and height
+    width,height =1000,1000
+
+    # Current rotation angle in degrees
+    angle = 0
+
+    def init():
+        """Initialize state"""
+        glEnable(GL_DEPTH_TEST)
+        glEnable(GL_LIGHTING)
+        glEnable(GL_LIGHT0)
+
+        # Helps with antialiasing
+        glEnable(GL_MULTISAMPLE)
+
+        # Texture initialization
+        textureId = loadTexture("youwin.png")
+        glEnable(GL_TEXTURE_2D)
+        global matrix 
+        matrix = glGetDoublev(GL_MODELVIEW_MATRIX)
+
+        
+    def display():
+        """Display callback: draw a sphere"""   
+        glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glMatrixMode (GL_MODELVIEW)
+        glLoadIdentity()
+        glTranslatef(0,0,-5)
+        # glRotatef(90, 0, 0, 1)
+        # glRotatef (80, 1,0,0)
+        # glRotatef (angle,0,1,1)
+        
+        # glutSolidSphere(1,40,50)
+        # glutSolidCube(1)
+        glMultMatrixd (matrix)
+        drawCube2(1)
+        
+        glutSwapBuffers()
+        
+    def reshape(w,h):
+        """Reshape Callback"""
+        glViewport(0,0,w,h)
+        global width,height
+        width,height = w,h
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        gluPerspective (60, w/h, 1, 10)
+
+    def idle():
+        """Idle callback. Rotate and redraw the globe"""
+        global angle
+        angle += 0.4
+        glutPostRedisplay()
     
-def display():
-    """Display callback: draw a sphere"""   
-    glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glMatrixMode (GL_MODELVIEW)
-    glLoadIdentity()
-    glTranslatef(0,0,-5)
-    glRotatef (-80, 1,0,0)
-    glRotatef (angle,0,1,1)
-    #glutSolidSphere(1,40,50)
-    #glutSolidCube(1)
-    drawCube()
-    glutSwapBuffers()
-    
-def reshape(w,h):
-    """Reshape Callback"""
-    glViewport(0,0,w,h)
-    global width,height
-    width,height = w,h
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    gluPerspective (60, w/h, 1, 10)
-
-def idle():
-    """Idle callback. Rotate and redraw the globe"""
-    global angle
-    angle += 0.4
-    glutPostRedisplay()
-    
-glutInit(sys.argv)
-glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_MULTISAMPLE)
-glutInitWindowSize (width,height)
-glutCreateWindow ("Textured Cube")
-glutDisplayFunc (display)
-glutReshapeFunc (reshape)
-glutIdleFunc(idle)
-init()
-glutMainLoop()
+    def mousepress (button, state, x, y):
+        if button == GLUT_LEFT_BUTTON:
+            if state == GLUT_DOWN:
+                global arcball
+                arcball = ArcBall ((width/2,height/2,0), width/2)
+                global startx, starty
+                startx, starty = x,y
+                glutMotionFunc (rotatecallback)
+            else:
+                glutMotionFunc (None)
+                
+    def rotatecallback (x, y):
+        global startx,starty,matrix
+        angle, axis = arcball.rot (startx, height - starty, x, height - y)
+        glLoadIdentity ()
+        glRotatef (degrees(angle),*axis)
+        glMultMatrixd (matrix)
+        matrix = glGetDoublev(GL_MODELVIEW_MATRIX)
+        startx,starty = x,y
+        glutPostRedisplay()
+        
+    glutInit(sys.argv)
+    glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_MULTISAMPLE)
+    glutInitWindowPosition((glutGet(GLUT_SCREEN_WIDTH)-width)//2,
+                        (glutGet(GLUT_SCREEN_HEIGHT)-height)//2)
+    glutInitWindowSize (width,height)
+    glutCreateWindow ("Textured Cube")
+    glutDisplayFunc (display)
+    glutReshapeFunc (reshape)
+    glutMouseFunc(mousepress)
+    # glutIdleFunc(idle)
+    init()
+    glutMainLoop()
